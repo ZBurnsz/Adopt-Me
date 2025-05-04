a2d8bd7460116b49cc6cda4eb3f8d0fce919fdd6
Zach Burns <Zachary@burnskc.com>
2025-05-03 21:33:04 -0500

    Updated the Comparator classes

--- src/main/java/edu/mizzou/adoptme/comparator/PetAgeComparator.java
+++ src/main/java/edu/mizzou/adoptme/comparator/PetAgeComparator.java
@@ -1,5 +1,12 @@
 package edu.mizzou.adoptme.comparator;
 
-public class PetAgeComparator {
+import edu.mizzou.adoptme.model.Pet;
+import java.util.Comparator;
 
+public class PetAgeComparator implements Comparator<Pet> {
+    @Override
+    public int compare(Pet p1, Pet p2) {
+        return Integer.compare(p1.getAge(), p2.getAge());
+    }
 }
+
--- src/main/java/edu/mizzou/adoptme/comparator/PetSpeciesComparator.java
+++ src/main/java/edu/mizzou/adoptme/comparator/PetSpeciesComparator.java
@@ -1,5 +1,11 @@
 package edu.mizzou.adoptme.comparator;
 
-public class PetSpeciesComparator {
+import edu.mizzou.adoptme.model.Pet;
+import java.util.Comparator;
 
+public class PetSpeciesComparator implements Comparator<Pet> {
+    @Override
+    public int compare(Pet p1, Pet p2) {
+        return p1.getClass().getSimpleName().compareTo(p2.getClass().getSimpleName());
+    }
 }
