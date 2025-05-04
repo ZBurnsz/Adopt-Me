f74b7127bf5d7c94a1e3a045cca9e30c4fef986e
Zach Burns <Zachary@burnskc.com>
2025-05-03 21:40:01 -0500

    Updated to match Json format

--- src/main/java/edu/mizzou/adoptme/model/ExoticAnimalDTO.java
+++ src/main/java/edu/mizzou/adoptme/model/ExoticAnimalDTO.java
@@ -1,24 +1,36 @@
 package edu.mizzou.adoptme.model;
 
+/**
+ * Data Transfer Object for exotic animals from third-party JSON format.
+ */
 public class ExoticAnimalDTO {
-	private String exoticName; 
-	private int exoticAge; 
-	private String type; 
-	
-	
-	Public ExoticAnimalDTO() {}
-	
-	public String getExoticName() { 
-		return exoticName; 
-	}
-	
-	public int getExoticAge() { 
-		return exoticAge; 
-	}
-	
-	public String getType() { 
-		return type; 
-	}
-	
-	
+    private String uniqueId;
+    private String animalName;
+    private String category;
+    private String subSpecies;
+    private int yearsOld;
+
+    // Required constructor for Gson
+    public ExoticAnimalDTO() {}
+
+    //getters matching expected Pet interface from json file
+    public String getExoticName() {
+        return animalName;
+    }
+
+    public int getExoticAge() {
+        return yearsOld;
+    }
+
+    public String getType() {
+        return category;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public String getSubSpecies() {
+        return subSpecies;
+    }
 }
