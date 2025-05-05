cb7daff51f4e0695921b7b0545565f10dc8c1259
Zach Burns <Zachary@burnskc.com>
2025-05-04 22:39:56 -0500

    Fixed the type error when viewing details of exotic pets.

--- src/main/java/edu/mizzou/adoptme/model/ExoticAnimalAdapter.java
+++ src/main/java/edu/mizzou/adoptme/model/ExoticAnimalAdapter.java
@@ -4,12 +4,12 @@ public class ExoticAnimalAdapter extends Pet {
     private ExoticAnimalDTO exotic;
 
     public ExoticAnimalAdapter(ExoticAnimalDTO exotic) {
-        super(exotic.getExoticName(), exotic.getExoticAge(), exotic.getType(), exotic.getExoticName());
+        super(exotic.getExoticName(), exotic.getExoticAge(), exotic.getType(), exotic.getSubSpecies());
         this.exotic = exotic;
     }
 
     @Override
     public String getDetails() {
-        return "I'm an exotic " + exotic.getType() + " named " + exotic.getExoticName() + ", age " + exotic.getExoticAge();
+        return "I'm an exotic " + exotic.getType() + " (" + exotic.getSubSpecies() + ") named " + exotic.getExoticName() + ", age " + exotic.getExoticAge();
     }
 }
